e5ab2a3d6d6964917c77ca5f17bb403d8182b701
Aayush shah <58617484+ios-shah@users.noreply.github.com>
2025-05-15 19:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -545,7 +545,6 @@ document.addEventListener("DOMContentLoaded", () => {
             adminEmail: document.getElementById("adminEmail").value,
         };
         localStorage.setItem("appSettings", JSON.stringify(newSettings));
-        alert("Settings saved successfully!");
     });
 });
 
